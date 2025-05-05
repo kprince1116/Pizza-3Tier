09fcbe8d35d115112d985321b39c8fb5222725ef
kprince116 <tatva.pca187@outlook.com>
2025-05-05 09:35:17 +0530

    commit

--- Pizzashop.Presentation/obj/Debug/net7.0/Pizzashop.Presentation.AssemblyInfo.cs
+++ Pizzashop.Presentation/obj/Debug/net7.0/Pizzashop.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizzashop.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2573fe9c0f470ebbbd9f76696e3f884b3bc373")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizzashop.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizzashop.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
